ffa76a98b9a81bfa2f93624cd7139b99a2f7a962
zhiyan114 <zhiyan114@furries.dev>
2025-10-13 19:56:35 -0400

    Updated MailService SDK

--- src/MailService.py
+++ src/MailService.py
@@ -1,4 +1,3 @@
-# SDK FROM: https://github.com/zhiyan114/email-queue
 from typing import TypedDict, Optional, Literal, Union, MutableSequence
 import requests
 import re
@@ -7,8 +6,9 @@ import re
 sendMailOpt = TypedDict(
     "sendMailOpt",
     {
-        "from": Optional[str],
+        "from": str,
         "to": Union[str, MutableSequence[str]],
+        "replyto": Optional[Union[str, MutableSequence[str]]],
         "subject": str,
         "text": Optional[str],
         "html": Optional[str]
@@ -26,6 +26,16 @@ class mailResFail(TypedDict):
     message: str
 
 
+class mailGetResItem(TypedDict):
+    id: int
+    fulfilled: Optional[str]
+    lasterror: Optional[str]
+
+
+class mailGetRes(TypedDict):
+    emails: MutableSequence[mailGetResItem]
+
+
 mailResType = Union[mailResPass, mailResFail]
 
 
@@ -41,8 +51,11 @@ class MailService:
 
     def sendMail(self, opt: sendMailOpt) -> Union[mailResType, str]:
         # General Validation
-        if (opt["from"] and not self.__validateMail(opt["from"])):
+        fromField = opt.get("from", None)
+        if (not fromField or not self.__validateMail(fromField)):
             raise Exception("sendMail: Invalid 'from' field")
+        if (not opt.get("to", None)):
+            raise Exception("sendMail: 'to' field is required")
         opt["to"] = opt["to"].split(",") if type(opt["to"]) is str else opt["to"]
         for to in opt["to"]:
             if (not self.__validateMail(to)):
@@ -50,25 +63,36 @@ class MailService:
         if (not opt["subject"]):
             raise Exception("sendMail: subject is required to avoid bad email delivery")
 
-        if (not opt["text"] and not opt["html"]):
+        if (opt.get("text", None) and not opt.get("html", None)):
             raise Exception("sendMail: Missing email body (either text or html is required)")
         if (opt.get("text", None) and opt.get("html", None)):
             raise Exception("sendMail: You cannot include both text and html email body")
 
+        optReplyTo = opt.get("replyto", None)
+        if optReplyTo is not None:
+            opt["replyto"] = optReplyTo.split(",") if type(optReplyTo) is str else optReplyTo
+            for rt in opt["replyto"]:
+                if (not self.__validateMail(rt)):
+                    raise Exception("sendMail: Invalid 'replyto' field was found")
+
         tRes = self.__transport("/requests", "POST", opt)
-        return tRes.json() if tRes.status_code == 200 else tRes.text
+        return tRes.json() if tRes.status_code in [200,422,503] else tRes.text
+
+    def getMailStat(self, reqID: str) -> Union[mailGetRes, str]:
+        tRes = self.__transport(f"/requests/{reqID}", "GET")
+        return tRes.json() if tRes.status_code in [200,422,503] else tRes.text
 
     def __validateMail(self, input: str):
         # Check "Name <email@address.local>"
-        if (re.match("^[a-zA-Z0-9 ._'`-]+ <[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}>$", input)):
+        if (re.match("^[a-zA-Z0-9 ._'`-]+ <[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}>$", input)):
             return True
 
         # Check "email@address.local"
-        if (re.match("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", input)):
+        if (re.match("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$", input)):
             return True
         return False
 
-    def __transport(self, path: str, method: str, jsonData: dict):
+    def __transport(self, path: str, method: str, jsonData: dict | None = None):
         header = {
             "Content-Type": "application/json",
             "Authorization": f"Bearer {self.apiKey}"
@@ -77,5 +101,7 @@ class MailService:
         match method.lower():
             case "post":
                 return requests.post(url=f"{self.baseUrl}{path}", headers=header, json=jsonData)
+            case "get":
+                return requests.get(url=f"{self.baseUrl}{path}", headers=header)
             case _:
                 raise Exception("SDK Error: Invalid/Unhandled HTTP method supplied")
